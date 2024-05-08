055397cb7b04c26ed7afa3b08ec348fdc196d1b3
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-05-08 12:58:07 +0300

    Добавьте файлы проекта.

---
+++ HealthCareApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthCareApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HealthCareApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HealthCareApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
